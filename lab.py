cd71a6345d0546c8f76f4729dbab0c4aa331cd9a
Mohamed567897 <120444332+Mohamed567897@users.noreply.github.com>
2023-02-10 18:25:35 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,52 @@
+'''def myInsertionsort(list):
+    for i in range(1,len(list)):
+        j=i-1
+        nextelement=list[i]
+        while(list[j]>n)and(j>=0):
+            list[j+1]=list[j]
+            j=j-1
+        list[j+1]=nextelement
+list[3,5,1,6,9]
+myInsertionsort(list)
+print(list)        '''
+
+
+
+'''def mySelectionSort(list):
+    for i in range (len(list)):
+        min=1
+        for j in range (i+1,len(list)):
+            if list[min]>list[j]:
+                min=j
+        list[i],list[min]=list[min],list[i]
+list=[3,5,2,1,6]
+mySelectionSort(list)
+print(list)'''
+
+'''n=[4,8,1,9,2,0]
+n.sort()
+print(n)'''
+
+'''c=['c','e','a','d','b']
+c.sort()
+print(c)'''
+
+'''d=[22.34,21.50,20.2,92.67]
+d.sort()
+print(d)'''
+
+'''s=['this','mohammed','is']
+s.sort()
+print(s)'''
+
+
+''''<head> <title>My Bootstrap CSS Example! </title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> </head>
+
+
+<!DOCTYPE html> <html lang="en"> <head> <title>Bootstrap Example</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> </head> <body>
+
+!DOCTYPE html> <html lang="en"> <head> <title>Bootstrap Example</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> </head> <body>
+
+
+<div class="container"> <h2>Simple Collapsible</h2> <p>Click on the button to toggle between showing and hiding content.</p> <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button> <div id="demo" class="collapse"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div> </div>'''
+
